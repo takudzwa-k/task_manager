06c4dd892b86b4c3eb10776425511655d2f51f98
Takudzwa Kachingwe <kachingwetakdzwa1@gmail.com>
2026-08-17 16:13:47 +0200

    creating json file & basic functions

---
+++ import json.py
@@ -0,0 +1,13 @@
+import json
+
+
+for id in range(1):
+    def addTask():
+        description = input("Enter a task: ")
+    def status():
+        status = input("Enter the status of the task: ")
+    def createdAt():
+        createdAt = input("Enter the creation date of the task: ")
+    def updatedAt():
+        updatedAt = input("Enter the last updated date of the task: ")
+    def 
